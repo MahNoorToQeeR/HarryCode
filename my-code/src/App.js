601f4aa5608af580c3687d3d26cad0b3b0d1f1d5
MahNooR ToQeeR <146191331+MahNoorToQeeR@users.noreply.github.com>
2024-10-22 15:33:15 +0500

    add

---
+++ my-code/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+      </div>
+    )
+  }
+}
+
+export default App
+
